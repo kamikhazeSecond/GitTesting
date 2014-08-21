9d4897fd5c70e7f7340890b160279f8b670d7d07
kamikhaze <prakashchettri87@gmail.com>
2014-08-22 00:44:53 +0530

    Added 2nd number

--- src/com/prakash/testgit/Home.java
+++ src/com/prakash/testgit/Home.java
@@ -25,7 +25,7 @@ public class Home extends ActionBarActivity {
                     .commit();
         }
         Toast.makeText(getApplicationContext(), "Add a 1st comment", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), "Add a comment", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Add a 2nd comment", Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(), "Add a 3rd comment", Toast.LENGTH_SHORT).show();
         
     }
